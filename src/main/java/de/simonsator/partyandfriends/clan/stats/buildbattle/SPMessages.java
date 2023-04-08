89782992ae9069a126473370c1ecd1c9a37339b2
Simonsator <00pflaume@gmail.com>
2023-04-08 15:41:28 +0200

    Updated messages

--- src/main/java/de/simonsator/partyandfriends/clan/stats/buildbattle/SPMessages.java
+++ src/main/java/de/simonsator/partyandfriends/clan/stats/buildbattle/SPMessages.java
@@ -18,7 +18,7 @@ public class SPMessages extends LanguageConfiguration {
 
 	private void loadDefaultValues() {
 		set("ClanStats.StatName", "Build Battle");
-		set("ClanStats.WinsLoseRatio", "&7The clan has win lose ratio of &a[WinsLosesRatio]&7.");
+		set("ClanStats.WinsLoseRatio", "&7The clan has a win lose ratio of &a[WinsLosesRatio]&7.");
 		set("ClanStats.Wins", "&7The people of the clan have won &a[WINS] &7games.");
 		set("ClanStats.Loses", "&7The people of the clan have lost &a[LOSES]&7 games.");
 	}
